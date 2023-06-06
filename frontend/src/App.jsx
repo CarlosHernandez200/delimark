f6c73ce38cdd86d892fca08a8207200cc685a2a0
Carlos Hernandez <carloshernandezc182@gmail.com>
2023-06-05 21:06:45 -0600

    Se agrega pages: - Login - NotFound

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/Header";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
@@ -9,6 +11,8 @@ const App = () => {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
