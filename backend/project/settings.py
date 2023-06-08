0203046750aa846a847312e1e5ec5b87c8f85d67
Carlos Hernandez <carloshernandezc182@gmail.com>
2023-06-07 23:19:19 -0600

    Se agrega el corsheaders para los cors

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     'rest_framework',
@@ -99,6 +100,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'project.urls'
@@ -172,6 +174,8 @@ STATIC_URL = 'static/'
 
 AUTH_USER_MODEL = 'api.User'
 
+# TODO: Permitir el acceso a todos los dominios
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
