0b1978997a317c008350e22c0a39c91b0b0c6dcd
Carlos Hernandez <carloshernandezc182@gmail.com>
2023-06-11 19:11:00 -0600

    Se remueve el AuthContext debido a que no esta usando

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,9 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import AuthProvider from "./context/AuthContext";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <AuthProvider>
-    <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(  
+    <>
       <App />
-    </React.StrictMode>
-  </AuthProvider>
+    </>  
 );
