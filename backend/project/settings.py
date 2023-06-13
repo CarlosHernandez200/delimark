f2055b2a97d4ed097b7027d5067be6812f10b30b
Carlos Hernandez <carloshernandezc182@gmail.com>
2023-06-13 17:44:26 -0600

    done vercel setting

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4c-g=b@6$y8#1--*3($@&6=w_m%v%d%fa792g4pl3y3*!(2*w!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
@@ -170,8 +170,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
+STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
 
 AUTH_USER_MODEL = 'api.User'
 
@@ -190,3 +191,5 @@ EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
+
+WSGI_APPLICATION = 'vercel_app.wsgi.app'
