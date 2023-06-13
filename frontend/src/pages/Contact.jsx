82c86e184b6feefdbf4ddd2d5913baf3585ffa71
Carlos Hernandez <carloshernandezc182@gmail.com>
2023-06-13 17:17:18 -0600

    Se agregar la funcion para enviar Correo de contacto

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,34 +1,75 @@
+import { Toaster, toast } from "sonner";
+import { useState } from "react";
+
 export const Contact = () => {
+  const [subject, setSubject] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch("http://127.0.0.1:8000/api/send-email/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          recipients: email,
+          subject: subject,
+          message: message,
+        }),
+      });
+
+      if (response.ok) {
+        console.log(response);
+        toast.success("correo enviado");
+      } else {
+        const errorData = await response.json();
+        toast.error(errorData.detail);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <section className="bg-white-100 py-10">
+      <Toaster richColors  />
       <div className="container mx-auto px-4">
-        <h2 className="text-3xl font-bold mb-6">Contacto</h2>
-        <div className="flex flex-wrap -mx-4">
-          <div className="w-full md:w-1/2 px-4 mb-6">
+        <h2 className="mb-6 text-3xl font-bold">Contacto</h2>
+        <form className="-mx-4 flex flex-wrap" onSubmit={handleSubmit}>
+          <div className="mb-6 w-full px-4 md:w-1/2">
             <input
               type="text"
               placeholder="Nombre"
-              className="w-full px-4 py-2 rounded border border-gray-300 focus:outline-none focus:border-blue-500"
+              onChange={(event) => setSubject(event.target.value)}
+              className="w-full rounded border border-gray-300 px-4 py-2 focus:border-blue-500 focus:outline-none"
             />
           </div>
-          <div className="w-full md:w-1/2 px-4 mb-6">
+          <div className="mb-6 w-full px-4 md:w-1/2">
             <input
               type="email"
               placeholder="Correo electrónico"
-              className="w-full px-4 py-2 rounded border border-gray-300 focus:outline-none focus:border-blue-500"
+              onChange={(event) => setEmail(event.target.value)}
+              className="w-full rounded border border-gray-300 px-4 py-2 focus:border-blue-500 focus:outline-none"
             />
           </div>
-          <div className="w-full px-4 mb-6">
+          <div className="mb-6 w-full px-4">
             <textarea
               placeholder="Mensaje"
-              className="w-full px-4 py-2 rounded border border-gray-300 focus:outline-none focus:border-blue-500"
+              className="w-full rounded border border-gray-300 px-4 py-2 focus:border-blue-500 focus:outline-none"
               rows="5"
+              onChange={(event) => setMessage(event.target.value)}
             ></textarea>
           </div>
           <div className="w-full px-4">
-            <button className="bg-blue-500 text-white px-6 py-2 rounded hover:bg-blue-600">Enviar</button>
+            <button className="rounded bg-blue-500 px-6 py-2 text-white hover:bg-blue-600">
+              Enviar
+            </button>
           </div>
-        </div>
+        </form>
       </div>
     </section>
   );
