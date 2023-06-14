dae3cb200c8b739ec4f8905eb31c68c0ef70b82b
Carlos Hernandez <carloshernandezc182@gmail.com>
2023-06-13 18:16:31 -0600

    Se modifica el vercel.json y build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -5,7 +5,8 @@ echo "Building the project..."
 python3.9 -m pip install -r requirements.txt
 
 echo "Make Migration..."
-python3.9 manage.py makemigrations --noinput
+python3.9 manage.py makemigrations api --noinput
+python3.9 manage.py makemigrations product --noinput
 python3.9 manage.py migrate --noinput
 
 echo "Collect Static..."
