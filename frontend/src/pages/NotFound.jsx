839bdf3e9f88ec0ac3b8c990426c7d83569555ef
Jose <jose1001819961@gmail.com>
2023-06-20 06:03:25 -0500

    se agrego la pagina de NotFound

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,7 +1,12 @@
 const NotFound = () => {
   return (
-    <div>NotFound</div>
-  )
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold mb-4">Error 404</h1>
+      <p className="text-lg text-gray-600">
+        La página que estás buscando no se encontró.
+      </p>
+    </div>
+  );
 }
 
-export default NotFound
+export default NotFound;
